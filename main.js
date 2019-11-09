bf06b5f5e461ad8b8b491b73698131af04811461
mankeip3531 <mankeip3531@hstat.org>
2019-11-09 15:32:16 -0600

    add player

--- main.js
+++ main.js
@@ -1,7 +1,12 @@
 function setup() {
-  createCanvas(400,400);
+  createCanvas(800, 800);
+  player.push(new Player(width/2,height/2,3))
 }
 
 function draw() {
-  background(0);
+  background(220);
+  for (let i= 0 ; i < player.length; i++){
+  player[i].move()
+  player[i].display()
+}
 }
---
+++ player.js
@@ -0,0 +1,30 @@
+var player = [];
+
+class Player {
+  constructor(x,y,speed) {
+    this.x = x;
+    this.y = y;
+    this.speed = speed;
+    this.move = function(){
+      this.x = constrain(this.x, 25, height - 25);
+      this.y = constrain(this.y, 25, height - 25);
+      if (keyIsDown(83)) {
+      // press w to move up
+        this.y += this.speed;
+      } else if (keyIsDown(87)) {
+        // press s to move  down
+        this.y -= this.speed;
+      }
+    if (keyIsDown(65)) {
+        // press a to move left
+        this.x -= this.speed;
+      } else if (keyIsDown(68)){
+        // press d to move right
+        this.x += this.speed;
+      }
+    }
+    this.display = function(){
+      ellipse(this.x, this.y, 50, 50);
+    }
+  }
+}
