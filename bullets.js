ca6c3faca682007145913e6b6d6179bab2f4a602
mankeip3531 <mankeip3531@hstat.org>
2019-11-09 22:34:38 -0600

    add player bullet

--- bullets.js
+++ bullets.js
@@ -15,3 +15,20 @@ class OpBullet {
     this.y -= this.yDisplacement / 60/this.speed;
   }
 }
+
+class PBullet {
+  constructor(x,y) {
+    this.x = x;
+    this.y = y;
+    this.speed= speed;
+    this.xDisplacement = (this.mouseX - this.x);
+    this.yDisplacement = (this.mouseY - this.y);
+    display() {
+      ellipse(this.x, this.y, 10)
+    }
+    move() {
+      this.x -= this.xDisplacement/60/this.speed
+      this.y -= this.yDisplacement/60/this.speed
+    }
+  }
+}
